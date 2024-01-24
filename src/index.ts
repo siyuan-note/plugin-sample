acc881c4081a5ed8a8b9b270ffffc68762362421
Vanessa <lly219@gmail.com>
2024-01-24 11:35:21 +0800

    :lipstick:

--- src/index.ts
+++ src/index.ts
@@ -145,8 +145,7 @@ export default class PluginSample extends Plugin {
                     dock.element.innerHTML = `<div class="fn__flex-1 fn__flex-column">
     <div class="block__icons">
         <div class="block__logo">
-            <svg><use xlink:href="#iconEmoji"></use></svg>
-            Custom Dock
+            <svg class="block__logoicon"><use xlink:href="#iconEmoji"></use></svg>Custom Dock
         </div>
         <span class="fn__flex-1 fn__space"></span>
         <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}"><svg><use xlink:href="#iconMin"></use></svg></span>
