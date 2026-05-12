f9c6cf22d26adf8a7e78d21271facf15ed8fee90
Vanessa <lly219@gmail.com>
2026-05-12 18:07:30 +0800

    :recycle: https://github.com/siyuan-note/plugin-sample/pull/42

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import {FlatCompat} from "@eslint/eslintrc";
 import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import globals from "globals";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {fileURLToPath} from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
